dcfef94764510e855430e6d38db091f5e8e5c8f2
KyleNdaka <kylendaka@gmail.com>
2026-09-13 20:49:44 -0300

    Update conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,5 +1,4 @@
 <?php
-
 $dbHost = getenv('DB_HOST') ?: $_ENV['DB_HOST'] ?? 'localhost';
 $dbUser = getenv('DB_USER') ?: $_ENV['DB_USER'] ?? 'root';
 $dbPass = getenv('DB_PASSWORD') ?: $_ENV['DB_PASSWORD'] ?? '';
@@ -8,9 +7,6 @@ $dbName = getenv('DB_NAME') ?: $_ENV['DB_NAME'] ?? 'assistencia_tecnica';
 $conexao = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
 
 if (!$conexao) {
-    $mensagemErroConexao = "Erro na conexão com o banco de dados: " . mysqli_connect_error();
-} else {
-    $mensagemErroConexao = null;
+    die("Erro na conexão com o banco de dados: " . mysqli_connect_error());
 }
-
 ?>
